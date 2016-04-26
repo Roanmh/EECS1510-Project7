ada60526dea34b811ff2c83246ef7be42a36f3f1
caleb-davenport <calebddd@gmail.com>
2016-04-26 19:50:32 -0400

    Reorganized several methods to clean up start() Added new Edit Menu Changed the default buttons to more closely match final functions

--- src/project7/InventoryManagementGUI.java
+++ src/project7/InventoryManagementGUI.java
@@ -20,51 +20,82 @@ import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.*;
+import javafx.scene.input.*;
 import javafx.scene.layout.*;
 import javafx.stage.Stage;
 
 public class InventoryManagementGUI extends Application {
-    private TableView<Entry> table = new TableView<Entry>();
-    private ObservableList<Entry> entryList = FXCollections.observableArrayList(
-    new Entry("Test", "12", "Note Test"));
+    private final static TableView<Entry> table = new TableView<Entry>();
+    private final static ObservableList<Entry> entryList
+            = FXCollections.observableArrayList(new Entry("Test", "12", "Note Test"));
+    private final static MenuBar menuBar = new MenuBar();
     
     @Override
     public void start(Stage primaryStage) {
-        MenuBar menuBar = new MenuBar();
-        Menu menuFile = new Menu("File");
-        menuBar.getMenus().addAll(menuFile);
+        entryList.add(new Entry());
         
-        TableColumn nameCol = new TableColumn("Name");
-        nameCol.setCellValueFactory(new PropertyValueFactory<Entry, String>("name"));
-        TableColumn<Entry, String> numberCol = new TableColumn("Number");
-        numberCol.setCellValueFactory(new PropertyValueFactory<Entry, String>("number"));
-        TableColumn notesCol = new TableColumn("Notes");
-        notesCol.setCellValueFactory(new PropertyValueFactory<Entry, String>("notes"));
-        
-        table.setItems(entryList);
-        table.getColumns().addAll(nameCol, numberCol, notesCol);
-        
-        VBox leftBox = new VBox();
-        leftBox.getChildren().addAll(new Button("Hello!"), new Button("Greetings!"), new Button("Welcome to Earth!"));
-        
-        HBox bottomBox = new HBox();
-        bottomBox.getChildren().addAll(new Button("Hello!"), new Button("Greetings!"), new Button("Welcome to Earth!"));
+        addMenus();
+        populateTable();
         
         VBox rightBox = new VBox();
-        rightBox.getChildren().addAll(new Button("Hello!"), new Button("Greetings!"), new Button("Welcome to Earth!"));
+        rightBox.getChildren().addAll(new Button("Add Entry"), new Button("Edit Entry"), new Button("Delete Entry"));
+        
+        HBox bottomBox = new HBox();
+        bottomBox.getChildren().addAll(new Button("Filter"));
         
         BorderPane root = new BorderPane();
         root.setTop(menuBar);
         root.setCenter(table);
-        root.setLeft(leftBox);
         root.setRight(rightBox);
         root.setBottom(bottomBox);
         
         Scene scene = new Scene(root, 800, 400);
         
-        primaryStage.setTitle("Hello World!");
+        primaryStage.setTitle("Inventory Management");
         primaryStage.setScene(scene);
         primaryStage.show();
+    }
+    private void addMenus() {
+        Menu menuFile = new Menu("File");
+        {
+            MenuItem newList = new MenuItem("New List");
+            newList.setAccelerator(KeyCombination.keyCombination("Ctrl+N"));
+
+            MenuItem openList = new MenuItem("Open List");
+            openList.setAccelerator(KeyCombination.keyCombination("Ctrl+O"));
+
+            MenuItem saveList = new MenuItem("Save List");
+            saveList.setAccelerator(KeyCombination.keyCombination("Ctrl+S"));
+
+            MenuItem exit = new MenuItem("Exit");
+            exit.setAccelerator(KeyCombination.keyCombination("Ctrl+Q"));
+
+            menuFile.getItems().addAll(newList, openList, saveList, new SeparatorMenuItem(), exit);
+        }
+        
+        Menu menuEdit = new Menu("Edit");
+        {
+            MenuItem addEntry = new MenuItem("Add Entry");
+
+            MenuItem editEntry = new MenuItem("Edit Entry");
+
+            MenuItem deleteEntry = new MenuItem("Delete Entry");
+
+            menuEdit.getItems().addAll(addEntry, editEntry, deleteEntry);
+        }
+        
+        menuBar.getMenus().addAll(menuFile, menuEdit);
+    }
+    private void populateTable() {
+        TableColumn nameCol = new TableColumn("Name");
+        nameCol.setCellValueFactory(new PropertyValueFactory<>("name"));
+        TableColumn<Entry, String> numberCol = new TableColumn("Number");
+        numberCol.setCellValueFactory(new PropertyValueFactory<>("number"));
+        TableColumn notesCol = new TableColumn("Notes");
+        notesCol.setCellValueFactory(new PropertyValueFactory<>("notes"));
+        
+        table.setItems(entryList);
+        table.getColumns().addAll(nameCol, numberCol, notesCol);  
     }
     public static void main(String[] args) {
         launch(args);
