93c7ad51cbcfdf940ebff60ccf1a6448152de1f3
roanmh <roanmh@gmail.com>
2016-04-27 20:08:55 -0400

    Added force entry and fixed addEntry to fix add entry flow. Also: 	-EntryReport: added addedSuccessfully boolean as explicit metric 	-Fixed main class attribute modifiers 	-Added documentation

--- src/project7/EntryReport.java
+++ src/project7/EntryReport.java
@@ -14,31 +14,39 @@ public class EntryReport {
     private final Entry FOUND_ENTRY;
     private final int FOUND_INDEX;
     private final boolean HAS_FOUND_ENTRY;
+    private final boolean ADDED_SUCCESFULLY;
     private final String ERROR_MESSAGE;
 
     public EntryReport(Entry attemptedEntry) {
-        this(attemptedEntry, new Entry(), -1, "");
+        this(attemptedEntry, new Entry(), -1, "", true);
     }
     
     public EntryReport(Entry attemptedEntry, String errMessage) {
-        this(attemptedEntry, new Entry(), -1, errMessage);
+        this(attemptedEntry, new Entry(), -1, errMessage, "".equals(errMessage));
     }
     
     public EntryReport(Entry attemptedEntry, Entry foundEntry, int foundIndex) {
-        this(attemptedEntry, foundEntry, foundIndex, "");
+        
+        // For addedSuccesfully: true if (no found entry && default index)
+        this(attemptedEntry, foundEntry, foundIndex, "",
+                (!foundEntry.exists() && (foundIndex == -1)));
     }
     
-    public EntryReport(Entry attemptedEntry, Entry foundEntry, int foundIndex, String errMessage) {
+    public EntryReport(Entry attemptedEntry, Entry foundEntry, int foundIndex,
+                       String errMessage, boolean addedSuccesfully) {
         this.ATTEMPTED_ENTRY = attemptedEntry;
         this.FOUND_ENTRY = foundEntry;
         this.HAS_FOUND_ENTRY = foundEntry.exists();
         this.FOUND_INDEX = foundIndex;
         this.ERROR_MESSAGE = errMessage;
+        this.ADDED_SUCCESFULLY = addedSuccesfully;
     }
     
     public Entry getAttemptedEntry() { return ATTEMPTED_ENTRY; }
     public Entry getFoundEntry() { return FOUND_ENTRY; }
     public int getFoundIndex() { return FOUND_INDEX; }
     public boolean hasFoundEntry() { return HAS_FOUND_ENTRY; }
+    public String getErrorMessage() { return ERROR_MESSAGE; }
+    public boolean addedSuccesfully() { return ADDED_SUCCESFULLY; }
 
 }
